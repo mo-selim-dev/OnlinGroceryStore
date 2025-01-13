51376fe29fbf84d59e404be43be733dd13fb667f
Mohammed Selim <mo.selim.dev@gmail.com>
2025-01-13 14:18:09 +0200

    Initial Commit

---
+++ OnlinGroceryStore/OnlinGroceryStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlinGroceryStoreApp.swift
+//  OnlinGroceryStore
+//
+//  Created by Mohamed Selim on 13/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct OnlinGroceryStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
