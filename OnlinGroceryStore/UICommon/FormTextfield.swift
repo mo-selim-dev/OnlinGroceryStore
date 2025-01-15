6c65cc414613fb070431f2e44836815443b0fef7
Mohammed Selim <mo.selim.dev@gmail.com>
2025-01-15 23:58:28 +0200

    Create LoginView and SignInView ,...

---
+++ OnlinGroceryStore/UICommon/FormTextfield.swift
@@ -0,0 +1,18 @@
+//
+//  FormTextfield.swift
+//  OnlinGroceryStore
+//
+//  Created by Mohamed Selim on 15/01/2025.
+//
+
+import SwiftUI
+
+struct FormTextfield: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FormTextfield()
+}
---
+++ OnlinGroceryStore/VeiwModel/MainViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewModel.swift
+//  OnlinGroceryStore
+//
+//  Created by Mohamed Selim on 15/01/2025.
+//
+
+import SwiftUI
+
+struct MainViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainViewModel()
+}
