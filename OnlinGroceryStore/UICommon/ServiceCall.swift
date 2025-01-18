31521bbc615522e1ed8f402e7ba7035725ffce8d
Mohammed Selim <mo.selim.dev@gmail.com>
2025-01-18 16:38:43 +0200

    Updat Code

---
+++ OnlinGroceryStore/UICommon/ServiceCall.swift
@@ -0,0 +1,18 @@
+//
+//  ServiceCall.swift
+//  OnlinGroceryStore
+//
+//  Created by Mohamed Selim on 16/01/2025.
+//
+
+import SwiftUI
+
+struct ServiceCall: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ServiceCall()
+}
