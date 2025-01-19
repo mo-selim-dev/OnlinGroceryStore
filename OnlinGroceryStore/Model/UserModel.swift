1ac4877964303547f08f52f6ba7901a138fd4c7c
Mohammed Selim <mo.selim.dev@gmail.com>
2025-01-19 05:31:43 +0200

    add nwe

---
+++ OnlinGroceryStore/Model/UserModel.swift
@@ -0,0 +1,35 @@
+//
+//  UserModel.swift
+//  OnlinGroceryStore
+//
+//  Created by Mohamed Selim on 19/01/2025.
+//
+
+
+import SwiftUI
+
+struct UserModel: Identifiable, Equatable {
+    
+    var id: Int = 0
+    var username: String = ""
+    var name: String = ""
+    var email: String = ""
+    var mobile: String = ""
+    var mobileCode: String = ""
+    var authToken: String = ""
+    
+    
+    init(dict: NSDictionary) {
+        self.id = dict.value(forKey: "user_id") as? Int ?? 0
+        self.username = dict.value(forKey: "username") as? String ?? ""
+        self.name = dict.value(forKey: "name") as? String ?? ""
+        self.email = dict.value(forKey: "email") as? String ?? ""
+        self.mobile = dict.value(forKey: "mobile") as? String ?? ""
+        self.mobileCode = dict.value(forKey: "mobile_code") as? String ?? ""
+        self.authToken = dict.value(forKey: "auth_token") as? String ?? ""
+    }
+    
+    static func == (lhs: UserModel, rhs: UserModel) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
