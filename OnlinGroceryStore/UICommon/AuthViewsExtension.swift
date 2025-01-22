599629e14298bce1931b8a216405407b2cb35c56
Mohammed Selim <mo.selim.dev@gmail.com>
2025-01-22 04:47:20 +0200

    Improving

---
+++ OnlinGroceryStore/UICommon/AuthViewsExtension.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  OnlinGroceryStore
+//
+//  Created by Mohamed Selim on 22/01/2025.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
