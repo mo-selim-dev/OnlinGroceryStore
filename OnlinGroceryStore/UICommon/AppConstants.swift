0569eddab8049493d03be6477cf1ca2906b53286
Mohammed Selim <mo.selim.dev@gmail.com>
2025-01-23 04:56:03 +0200

    u3

--- OnlinGroceryStore/UICommon/AppConstants.swift
+++ OnlinGroceryStore/UICommon/AppConstants.swift
@@ -8,7 +8,7 @@
 
 import SwiftUI
 
-struct Globs {
+struct AppConstants {
     static let AppName = "Online Groceries"
     
     static let BASE_URL = "http://localhost:3001/api/app/"
---
+++ OnlinGroceryStore/View/MainTab/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  OnlinGroceryStore
+//
+//  Created by Mohamed Selim on 23/01/2025.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
