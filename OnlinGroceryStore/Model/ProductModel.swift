e27978da100ab3c0ab6943edc66ed7f1ecfde984
Mohammed Selim <mo.selim.dev@gmail.com>
2025-01-26 03:59:43 +0200

    Update

---
+++ OnlinGroceryStore/Model/ProductModel.swift
@@ -0,0 +1,18 @@
+//
+//  ProductModel.swift
+//  OnlinGroceryStore
+//
+//  Created by Mohamed Selim on 24/01/2025.
+//
+
+import SwiftUI
+
+struct ProductModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductModel()
+}
---
+++ OnlinGroceryStore/Model/TypeModel.swift
@@ -0,0 +1,18 @@
+//
+//  TypeModel.swift
+//  OnlinGroceryStore
+//
+//  Created by Mohamed Selim on 25/01/2025.
+//
+
+import SwiftUI
+
+struct TypeModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TypeModel()
+}
