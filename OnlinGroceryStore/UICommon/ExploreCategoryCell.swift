111d2eb97860c4ee90d5c9d1d265c294741a9322
Mohammed Selim <mo.selim.dev@gmail.com>
2025-01-29 01:19:49 +0200

    u3.3

---
+++ OnlinGroceryStore/UICommon/ExploreCategoryCell.swift
@@ -0,0 +1,18 @@
+//
+//  ExploreCategoryCell.swift
+//  OnlinGroceryStore
+//
+//  Created by Mohamed Selim on 29/01/2025.
+//
+
+import SwiftUI
+
+struct ExploreCategoryCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExploreCategoryCell()
+}
