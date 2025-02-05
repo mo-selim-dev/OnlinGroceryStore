4d96e415db85cbf9018c0f19f821095f46e2d19b
Mohammed Selim <mo.selim.dev@gmail.com>
2025-02-05 07:28:09 +0200

    u5

---
+++ OnlinGroceryStore/UICommon/CartItemRow.swift
@@ -0,0 +1,18 @@
+//
+//  CartItemRow.swift
+//  OnlinGroceryStore
+//
+//  Created by Mohamed Selim on 04/02/2025.
+//
+
+import SwiftUI
+
+struct CartItemRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CartItemRow()
+}
---
+++ OnlinGroceryStore/View/Account/AccountView.swift
@@ -0,0 +1,18 @@
+//
+//  AccountView.swift
+//  OnlinGroceryStore
+//
+//  Created by Mohamed Selim on 05/02/2025.
+//
+
+import SwiftUI
+
+struct AccountView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AccountView()
+}
