f06116275ef39fc30d39c8010c8b5fa0046a5a0e
Mohammed Selim <mo.selim.dev@gmail.com>
2025-02-06 18:56:31 +0200

    U5.3

---
+++ OnlinGroceryStore/VeiwModel/PromoCodeViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  PromoCodeViewModel.swift
+//  OnlinGroceryStore
+//
+//  Created by Mohamed Selim on 06/02/2025.
+//
+
+import SwiftUI
+
+struct PromoCodeViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PromoCodeViewModel()
+}
---
+++ OnlinGroceryStore/View/CartView/CheckoutView.swift
@@ -0,0 +1,18 @@
+//
+//  CheckoutView.swift
+//  OnlinGroceryStore
+//
+//  Created by Mohamed Selim on 06/02/2025.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckoutView()
+}
