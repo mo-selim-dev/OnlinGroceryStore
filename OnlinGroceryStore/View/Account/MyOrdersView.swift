8f2fb57a85a7fd484b41ea64ffd0271f60f38c06
Mohammed Selim <mo.selim.dev@gmail.com>
2025-02-11 18:57:20 +0200

    U5.9

---
+++ OnlinGroceryStore/View/Account/MyOrdersView.swift
@@ -0,0 +1,18 @@
+//
+//  MyOrdersView.swift
+//  OnlinGroceryStore
+//
+//  Created by Mohamed Selim on 11/02/2025.
+//
+
+import SwiftUI
+
+struct MyOrdersView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyOrdersView()
+}
